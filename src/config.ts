d2dee62c16d14bcdfea3b846f57c2f689579f8c8
kautuk kundan <kautukkundan@gmail.com>
2021-08-27 01:48:10 +0530

    fix: trailing slash in URL

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ import genesis from "./genesis.json";
 
 const config= {
     GENESIS: genesis,
-    BASE_URL: "https://goerli-proposer.thehubbleproject.net/"
+    BASE_URL: "https://goerli-proposer.thehubbleproject.net"
 }
 
 export default config;
