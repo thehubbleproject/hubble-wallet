e2bd86fdb0ed975631b4fde994c7e3f081b152cf
kautuk kundan <kautukkundan@gmail.com>
2021-08-27 01:11:58 +0530

    added env config for local and prod

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,18 @@
-import genesis from "./genesis.json";
+import genesis_goerli from "./genesis/genesis-goerli.json";
+import genesis_local from "./genesis/genesis-local.json";
 
-const config= {
-    GENESIS: genesis,
-    BASE_URL: "https://goerli-proposer.thehubbleproject.net/"
+const getGenesisConfig = () => {
+    if(process.env.REACT_APP_NETWORK === 'goerli') {
+        return genesis_goerli
+    } else if(process.env.REACT_APP_NETWORK === 'local') {
+        return genesis_local
+    }
+    return genesis_local
+}
+
+const config = {
+    GENESIS: getGenesisConfig(),
+    BASE_URL: process.env.REACT_APP_PROPOSER_URL
 }
 
 export default config;
